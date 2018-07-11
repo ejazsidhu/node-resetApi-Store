b38dd3df3b5b7c95d17fadbca1f9b6151a8423a1
ejazsidhu@gmail.com <ejazsidhu@gmail.com>
2018-07-11 13:43:39 +0500

    bory parser added and cors is added

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,13 +8,34 @@ router.get("/", (req, res, next) => {
     })
 
 });
+router.get("/:productId", (req, res, next) => {
+    const id = req.params.productId;
+    res.status(200).json({
+        message: `GET method with ${id}.`
+    })
+
+});
 router.post("/", (req, res, next) => {
 
+    const product = {
+        name:req.body.name,
+        price:req.body.price
+
+    }
+
+    res.status(201).json({
+        message: "product created successfully.",
+        createdProduct: product
+    })
+
+});
+router.patch("/:productId", (req, res, next) => {
+    const id = req.params.productId;
     res.status(200).json({
-        message: "POST method of proiducts"
+        message: `PATCH method with ${id}.`
     })
 
 });
 
-module.exports=router;
+module.exports = router;
 
--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-const express=require("express");
-const app=express();
-const productRoutes=require("./api/routes/products");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-app.use("/products",productRoutes)
 
-// app.use((req,res,next)=>{
-//     res.status(200).json({
-//         message:"api works"
-//     })
-// })
-module.exports=app;
+const productRoutes = require("./api/routes/products");
+const ordersRoutes = require("./api/routes/orders");
+
+
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Auth-token,X-Request-with,Content-Type,Accept,Authorization");
+
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Headers", "POST,PUT,PATCH,DELETE,GET");
+        return res.status(200).json();
+    }
+});
+app.use("/products", productRoutes);
+app.use("/orders", ordersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+
+    res.status(error.status || 500);
+
+    res.json({
+        error: {
+            message: error.message,
+            status: error.status
+        }
+    });
+
+});
+
+
+module.exports = app;
